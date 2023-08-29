43cde3d0696c2f126b7e36bdd90552164a7c8fec
D.K <6912915+lwinch2006@users.noreply.github.com>
2023-08-29 19:59:15 +0200

    Added code for Chapter 8

---
+++ chapter6_1/bin/chapter6_1.dart
@@ -0,0 +1,5 @@
+import 'package:chapter6_1/chapter6_1.dart' as chapter6_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${chapter6_1.calculate()}!');
+}
---
+++ chapter8/bin/main.dart
@@ -0,0 +1,6 @@
+import "package:chapter8/core.dart" as corelib;
+
+void main() {
+  corelib.greeting();
+  corelib.printRandomNumber();
+}
